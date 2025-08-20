a2df0b55a3969ea9f1f251d05cee0529b6a330b6
nahyln <elinn785@gmail.com>
2025-08-19 22:51:35 -0400

    Add src/components/TopNav.js 1755658295984

---
+++ src/components/TopNav.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const TopNav = () => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-dark"  style={{  backgroundImage: `url(./topnav.png)`, backgroundSize: "100% 100%", backgroundRepeat: "no-repeat", height: "120px", boxShadow: "3px 3px 3px #777"}}>
+      <div className="container-fluid">
+      </div>
+    </nav>
+  );
+};
+
+export default TopNav;
