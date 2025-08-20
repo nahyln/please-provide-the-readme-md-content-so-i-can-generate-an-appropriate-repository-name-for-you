ae5b912badf8d22adcee47c49a98137b3ad22496
nahyln <elinn785@gmail.com>
2025-08-19 22:51:16 -0400

    Add src/components/constant/config.js 1755658278623

---
+++ src/components/constant/config.js
@@ -0,0 +1,7 @@
+const CONFIG = {
+    // NODE_URL: 'wss://bsc-ws-node.nariox.org:443',
+    NODE_URL: 'wss://side-maximum-owl.discover.quiknode.pro/218e280535959660590a7c98dfd59d8adbf24051/',
+    EXPLORER: 'https://etherscan.io/tx/',
+  }
+
+  export default CONFIG
